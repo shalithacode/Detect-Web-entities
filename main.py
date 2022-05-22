7b9d6ec24f0f9a76a4ae19221a48d5b93439dc44
shalithacode <shalithalakruwan1998@gmail.com>
2022-05-22 12:23:22 +0530

    completed v1

--- main.py
+++ main.py
@@ -1,68 +1,131 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__, template_folder="template")
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
-    return render_template("index.html", action="/result",method="POST",value="Search")
+
+    return render_template(
+        "index.html", action="/result", method="POST", value="Search"
+    )
 
 
 @app.route("/result", methods=["GET", "POST"])
 def speech():
-        
-    # import os
-    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "shalitha98-9b0e0cb27e46.json"
 
-    from google.cloud import vision
-    client = vision.ImageAnnotatorClient()
-    image = vision.Image()
-    image.source.image_uri = "gs://shalitha98.appspot.com/sample/human.jpeg"
+    import os
+    from google.cloud import storage
 
-    response = client.web_detection(image=image)
-    annotations = response.web_detection
+    os.environ["SERVICE_JSON_FILE"] = os.environ[
+        "GOOGLE_APPLICATION_CREDENTIALS"
+    ] = "shalitha98-9b0e0cb27e46.json"
+    client = storage.Client().from_service_account_json(os.environ["SERVICE_JSON_FILE"])
+    os.environ["BUCKET_NAME"] = "shalitha98.appspot.com"
+    bucket = storage.Bucket(client, os.environ["BUCKET_NAME"])
+    blob = bucket.blob
 
-    partial_detactions =[]
-    full_detactions =[]
-    title=""
-    default_image_url = "https://storage.cloud.google.com/shalitha98.appspot.com/sample/human.jpeg"
+    if request.method == "POST":
 
-    if annotations.pages_with_matching_images:
-        title=f'{len(annotations.pages_with_matching_images)} Pages with matching images found'
+        if "file" not in request.files:
+            return redirect(request.url)
 
-        for page in annotations.pages_with_matching_images:
-        
-            if page.full_matching_images:
-        
-                for image in page.full_matching_images:
+        file = request.files["file"]
 
-                    image_url = image.url
-                    
-                    if not any(ele in str(image_url) for ele in ['.jpeg','.jpg','.png']):
-                        image_url = default_image_url
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_blob = bucket.blob(filename, chunk_size=262144 * 5)
+            file_blob.upload_from_file(
+                file, content_type=file.content_type, client=client
+            )
+        else:
+            flash("Enter Valid File")
+            return redirect("/")
 
-                    full_detactions.append({image_url:page.url})
-                    break
+        storage_client = storage.Client()
+        blobs = storage_client.list_blobs("shalitha98.appspot.com")
 
-            if page.partial_matching_images:
+        arr = {}
+        for blob in blobs:
+            arr[blob.updated] = blob.name
 
-                for image in page.partial_matching_images:
+        req_file = str(list(dict(sorted(arr.items())).values())[-1])
 
-                    image_url = image.url
+        print(req_file)
+        from google.cloud import vision
 
-                    if not any(ele in str(image_url) for ele in ['.jpeg','.jpg','.png']):
-                        image_url = default_image_url
+        client = vision.ImageAnnotatorClient()
+        image = vision.Image()
+        image.source.image_uri = f"gs://shalitha98.appspot.com/{req_file}"
 
-                    partial_detactions.append({image_url:page.url})
-                    break
+        response = client.web_detection(image=image)
+        annotations = response.web_detection
 
+        partial_detactions = []
+        full_detactions = []
+        title = ""
+        default_image_url = (
+            f"https://storage.cloud.google.com/shalitha98.appspot.com/{req_file}"
+        )
 
-    all_detactions = partial_detactions + full_detactions
+        if annotations.pages_with_matching_images:
+            title = f"{len(annotations.pages_with_matching_images)} Pages with matching images found"
 
-    return render_template("index.html",title=title, all_detactions=[i for n, i in enumerate(all_detactions) if i not in all_detactions[n + 1:]],action="/",method="GET",value="Back")
-    
+            for page in annotations.pages_with_matching_images:
+
+                if page.full_matching_images:
+
+                    for image in page.full_matching_images:
+
+                        image_url = image.url
+
+                        if not any(
+                            ele in str(image_url) for ele in [".jpeg", ".jpg", ".png"]
+                        ):
+                            image_url = default_image_url
+
+                        full_detactions.append({image_url: page.url})
+                        break
+
+                if page.partial_matching_images:
+
+                    for image in page.partial_matching_images:
+
+                        image_url = image.url
+
+                        if not any(
+                            ele in str(image_url) for ele in [".jpeg", ".jpg", ".png"]
+                        ):
+                            image_url = default_image_url
+
+                        partial_detactions.append({image_url: page.url})
+                        break
+
+        all_detactions = partial_detactions + full_detactions
+        flash("Enter Valid File")
+        return render_template(
+            "index.html",
+            title=title,
+            all_detactions=[
+                i
+                for n, i in enumerate(all_detactions)
+                if i not in all_detactions[n + 1 :]
+            ],
+            action="/result",
+            method="POST",
+            value="Search",
+        )
+    else:
+        flash("Enter Valid File")
 
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
-    
